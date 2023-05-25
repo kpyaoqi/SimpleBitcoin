134a51f9d6bc19b1a2fb24c5e5b159e47b24f35c
kpyaoqi <yaoqi2136712851@gmail.com>
2023-05-25 22:31:43 +0800

    yaoqi

--- src/demo6_transaction2/block.go
+++ src/demo6_transaction2/block.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"crypto/sha256"
 	"encoding/gob"
 	"fmt"
 	"time"
@@ -34,13 +33,12 @@ func NewBlock(transactions []*Transaction, prevBlockHash []byte) *Block {
 
 // 计算区块里所有交易的哈希
 func (b *Block) HashTransactions() []byte {
-	var txHashes [][]byte
-	var txHash [32]byte
+	var transactions [][]byte
 	for _, tx := range b.Transactions {
-		txHashes = append(txHashes, tx.ID)
+		transactions = append(transactions, tx.Serialize())
 	}
-	txHash = sha256.Sum256(bytes.Join(txHashes, []byte{}))
-	return txHash[:]
+	mTree := NewMerkleTree(transactions)
+	return mTree.RootNode.Data
 }
 
 // 对Block结构进行序列化
---
+++ src/demo6_transaction2/merkle_tree.go
@@ -0,0 +1,56 @@
+package main
+
+import "crypto/sha256"
+
+type MerkleTree struct {
+	RootNode *MerkleNode
+}
+
+type MerkleNode struct {
+	Left  *MerkleNode
+	Right *MerkleNode
+	Data  []byte
+}
+
+//创建一个新的结点
+func NewMerkleNode(left, right *MerkleNode, data []byte) *MerkleNode {
+	mNode := MerkleNode{}
+	if left != nil && right != nil {
+		hash := sha256.Sum256(data)
+		mNode.Data = hash[:]
+	} else {
+		prevHashes := append(left.Data, right.Data...)
+		hash := sha256.Sum256(prevHashes)
+		mNode.Data = hash[:]
+	}
+	mNode.Left = left
+	mNode.Right = right
+	return &mNode
+}
+
+//创建一一棵Merkle树
+func NewMerkleTree(data [][]byte) *MerkleTree {
+	var nodes []MerkleNode
+	for _, datum := range data {
+		node := NewMerkleNode(nil, nil, datum)
+		nodes = append(nodes, *node)
+	}
+	for len(nodes) != 1 {
+		nodes = NewMerkleTreeLevel(nodes)
+	}
+	mTree := MerkleTree{&nodes[0]}
+	return &mTree
+}
+
+//构建MerkleTree的一层结点
+func NewMerkleTreeLevel(nodes []MerkleNode) []MerkleNode {
+	if len(nodes)%2 != 0 {
+		nodes = append(nodes, nodes[len(nodes)-1])
+	}
+	var newLevel []MerkleNode
+	for j := 0; j < len(nodes); j += 2 {
+		node := NewMerkleNode(&nodes[j], &nodes[j+1], nil)
+		newLevel = append(newLevel, *node)
+	}
+	return newLevel
+}
